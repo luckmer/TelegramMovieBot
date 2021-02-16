98a04803ee3c615c96ec0f7981b7203b07e0e6ff
luckmer <woszczo04@gmail.com>
2021-02-16 16:16:24 +0100

    better view

--- views/Result.js
+++ views/Result.js
@@ -1,14 +1,20 @@
-const Result = (movie) =>{
+const Result = (movie) => {
     return (
-        "Title: " + `_${movie.Title}_` +
-        "\nYear: " + `_${movie.Year}_` +
-        "\nRated: " + `_${movie.Rated}_` +
-        "\nReleased: " + `_${ movie.Released }_` +
+        "Title: " +
+        `_${movie.Title}_` +
+        "\nYear: " +
+        `_${movie.Year}_` +
+        "\nRated: " +
+        `_${movie.Rated}_` +
+        "\nReleased: " +
+        `_${movie.Released}_` +
         "\n" +
-        "\nProduction: " + `_${ movie.Production }_` +
+        "\nProduction: " +
+        `_${movie.Production}_` +
         "\n" +
-        "\nPlot: " + `_${ movie.Plot }_`
-    )
-}
+        "\nPlot: " +
+        `_${movie.Plot}_`
+    );
+};
 
 module.exports = Result;
