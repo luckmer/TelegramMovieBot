813e0d6696eed119975fab386b7cb5a01cf8dea9
luckmer <woszczo04@gmail.com>
2021-02-16 16:13:39 +0100

    commands

---
+++ views/commands.js
@@ -0,0 +1,9 @@
+const Commands =
+    "find movie : " +
+    " /movie + movie" +
+    "\nweather : " +
+    " /weather + city " +
+    "\nweather for 3 days : " +
+    " /weatherweek + city ";
+
+module.exports = Commands;
